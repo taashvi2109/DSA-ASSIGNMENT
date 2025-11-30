c475e755e1ac38b5f2096f9098b7bceb8dc9c8b5
Taashvi Aggarwal <taashviaggarwal06@gmail.com>
2025-11-30 23:32:45 +0530

    ques4.cpp

---
+++ assignment 3/ques4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int precedence(char c) {
+    if (c == '^') return 3;
+    if (c == '*' || c == '/') return 2;
+    if (c == '+' || c == '-') return 1;
+    return -1;
+}
+
+int main() {
+    string infix, postfix = "";
+    cin >> infix;
+
+    stack<char> st;
+
+    for (char c : infix) {
+        if (isalnum(c))
+            postfix += c;
+        else if (c == '(')
+            st.push(c);
+        else if (c == ')') {
+            while (!st.empty() && st.top() != '(') {
+                postfix += st.top();
+                st.pop();
+            }
+            st.pop();
+        } else {
+            while (!st.empty() && precedence(st.top()) >= precedence(c)) {
+                postfix += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while (!st.empty()) {
+        postfix += st.top();
+        st.pop();
+    }
+
+    cout << postfix;
+}
