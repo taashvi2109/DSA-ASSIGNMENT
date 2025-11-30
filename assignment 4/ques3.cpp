80c5d31a2de2be0439d68e99c36a83089aa5d7f0
Taashvi Aggarwal <taashviaggarwal06@gmail.com>
2025-11-30 23:48:15 +0530

    ques3.cpp

---
+++ assignment 4/ques3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    int n, x;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        q.push(x);
+    }
+
+    queue<int> first;
+    int half = n / 2;
+
+    for (int i = 0; i < half; i++) {
+        first.push(q.front());
+        q.pop();
+    }
+
+    while (!first.empty()) {
+        q.push(first.front());
+        first.pop();
+        q.push(q.front());
+        q.pop();
+    }
+
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+}
